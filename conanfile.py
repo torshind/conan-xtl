6b29d88608f361a91bc9545aa994d1376e42efec
Marcello Mansueto <mvm@mydatamodels.com>
2019-06-12 15:19:59 +0200

    Removed git tools

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,12 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
 
 
 class XtlConan(ConanFile):
     name = "xtl"
     version = "0.6.4"
     license = "BSD 3-Clause"
-    url = "https://github.com/QuantStack/xtl"
+    homepage = "https://github.com/QuantStack/xtl/"
+    url = "https://github.com/torshind/conan-xtl/"
     description = "The x template library"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
@@ -14,17 +15,17 @@ class XtlConan(ConanFile):
     exports_sources = "*"
 
     def source(self):
-        git = tools.Git()
-        git.clone("https://github.com/QuantStack/xtl.git", self.version)
+        self.run("git clone --branch "
+                 + self.version
+                 + " https://github.com/QuantStack/xtl.git")
 
     def build(self):
         pass
 
     def package(self):
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(source_folder="xtl")
         cmake.install()
 
     def package_info(self):
         self.info.header_only()
-        self.cpp_info.libs = ["xtl"]
